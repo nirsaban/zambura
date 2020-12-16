c313189046148b86b20f0773331aafdf5a716e45
nirsa11 <nir@mobile-brain.net>
2020-12-16 11:14:15 +0200

    config

--- admin/test.php
+++ admin/test.php
@@ -84,11 +84,11 @@ if ($_GET['del'])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/vendor/animate.css">
         <link rel="stylesheet" href="assets/css/vendor/font-awesome.min.css">
         <link rel="stylesheet" href="assets/js/vendor/animsition/css/animsition.min.css">
-        <link rel="stylesheet" href="assets/js/vendor/datatables/css/jquery.dataTables.min.css">
+        <!-- <link rel="stylesheet" href="assets/js/vendor/datatables/css/jquery.dataTables.min.css">
         <link rel="stylesheet" href="assets/js/vendor/datatables/datatables.bootstrap.min.css">
         <link rel="stylesheet" href="assets/js/vendor/datatables/extensions/ColReorder/css/dataTables.colReorder.min.css">
         <link rel="stylesheet" href="assets/js/vendor/datatables/extensions/Responsive/css/dataTables.responsive.css">
