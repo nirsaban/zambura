621ded83d3711d0e69644109ec4305bb35100ef0
nirsa11 <nir@mobile-brain.net>
2020-12-16 10:24:48 +0200

    config

--- php/db.php
+++ php/db.php
@@ -1,10 +1,10 @@
 <?php
 
 	$dbhost = "127.0.0.1";
-	$dbuser = "onclickj_zambura";
+	$dbuser = "onclickj_main";
 	$dbpass = "parnasa2020";
 		
-	$con = mysqli_connect($dbhost, $dbuser, $dbpass,'zambura');
+	$con = mysqli_connect($dbhost, $dbuser, $dbpass,'onclickj_zambura');
 	 if(! $con )
     	{
 	  die('Could not connect: ' . mysqli_error($con));
