97648be88f4cf2fc76338e36602fc9bec346ac3e
nirsa11 <nir@mobile-brain.net>
2020-12-16 10:49:01 +0200

    config

--- admin/question_catagory.php
+++ admin/question_catagory.php
@@ -544,7 +544,7 @@ if ($_POST['catindex'] !="0")
 <!-- ============================================
 ============== Vendor JavaScripts ===============
 ============================================= -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> -->
 <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery/jquery-1.11.2.min.js"><\/script>')</script>
 
 <script src="assets/js/vendor/bootstrap/bootstrap.min.js"></script>
